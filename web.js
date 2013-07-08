31441218e0517293c2bdd43ac034e7f9df6b0c60
Jhon Smith <example@standford.edu>
2013-07-08 19:39:41 +0000

    first try

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var fs = require('fs');
+  var infile = "index.html";
+  contents = fs.readFileSync(infile, "ascii");
+  response.send(contents);
 });
 
 var port = process.env.PORT || 5000;
